46658d8926aec45f6f9acb99e7dcd8739a82e92a
Pokey Rule <pokey.rule@globality.com>
2019-06-13 11:29:09 +0100

    Add long_description to setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -8,6 +8,8 @@ setup(
     name=project,
     version=version,
     description="{{cookiecutter.short_description}}",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="{{cookiecutter.author}}",
     author_email="{{cookiecutter.author_email}}",
     url="https://github.com/{{cookiecutter.organization_name}}/{{cookiecutter.repo_name}}",
